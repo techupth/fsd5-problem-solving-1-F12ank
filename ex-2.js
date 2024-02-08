af5b510ebd959e556865710d779b54c10b06b641
nattawat <frank.nattawat@hotmail.com>
2024-02-08 15:43:26 +0700

    feat:complete ex

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,27 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
+  let setString = s.replace(/\s|:|,/g, "");
+  console.log(setString);
+  let word = setString.split("");
+  console.log(word);
+  let reverseText = word.reverse();
+  console.log(reverseText);
+  let joinText = reverseText.join("");
+  console.log(joinText);
+  let lowerText = joinText.toLowerCase();
+  console.log(lowerText);
+
+  let defaultWord = word;
+  let joinDefaultText = defaultWord.join("");
+  console.log(joinDefaultText);
+  let lowerDefaultText = joinDefaultText.toLowerCase();
+
+  if (lowerText === lowerDefaultText) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
